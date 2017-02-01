9db47aa10f43ad0b555bda4ac14f6c318ad82f15
THE-COB <jmlife6@gmail.com>
2017-01-31 21:39:54 -0500

    FINISHED JS
    the alerts just need to be made into something more practical.

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-<script type="text/javascript">
-var choice = [];
+var options = ["Azio MGK1-K with Blues", "Atreus with Blues", "CM Storm QuickFire TK", "IBM Model M", "Planck with Browns", "IBM Model M SSK", "Corsair STRAFE with Reds", "Planck with Reds", "Corsair Vengeance K65 with Reds", "Das Professional 4 with Blues", "Planck with Blues", "POK3R ANSI US with Blues", "Das Professional 4 with Browns", "Mistel Barocco Ergonomic Split", "POK3R ANSI US with Browns", "Corsair K70", "Planck with Reds", "POK3R ANSI US with Reds", "Matias Tactile Pro with Clicky Switches", "Ergodox Infinity with Greens", "HHKB 2", "Matias Tactile Pro with Clicky Switches", "Ergodox Infinity with zealio swiches", "WhiteFox with Greens", "Cherry MX 6.0 with Reds", "Ergodox Infinity with Reds", "Carpe Keybaords JD45 with Blacks"];
 	function q1(answer){
 		switch(answer){
 			case 1:
@@ -13,7 +12,8 @@ var choice = [];
 			document.getElementById("three").innerHTML = "Gamer";
 			var a3 = document.getElementById("three");
 			a3.setAttribute("onclick", "q2(3)");
-			choice.push(1);
+			options.splice(9, 27);
+			alert(options);
 			break;
 			
 			case 2:
@@ -26,8 +26,9 @@ var choice = [];
 			a2.setAttribute("onclick", "q2(2)");
 			document.getElementById("three").innerHTML = "Gamer";
 			var a3 = document.getElementById("three");
-			a3.setAttribute("onclick", "q2(3)");	
-			choice.push(2);
+			a3.setAttribute("onclick", "q2(3)");
+			options.splice(0, 9);
+			options.splice(9, 18);
 			break;
 			
 			case 3:
@@ -41,12 +42,12 @@ var choice = [];
 			document.getElementById("three").innerHTML = "Gamer";
 			var a3 = document.getElementById("three");
 			a3.setAttribute("onclick", "q2(3)");
-			choice.push(3);
+			options.splice(0, 18);
 			break;
 		}
 		
 	}
-		
+	
 	function q2(answer){
 		switch(answer){
 			case 1:
@@ -60,8 +61,7 @@ var choice = [];
 			document.getElementById("three").innerHTML = "Very Small";
 			a1 = document.getElementById("three");
 			a1.setAttribute("onclick", "q3(3)");
-			choice.push(1);
-			alert(choice);
+			options.splice(3, 9);
 			break;
 			
 			case 2:
@@ -75,8 +75,8 @@ var choice = [];
 			document.getElementById("three").innerHTML = "Very Small";
 			a1 = document.getElementById("three");
 			a1.setAttribute("onclick", "q3(3)");
-			choice.push(2);
-			alert(choice);
+			options.splice(0, 3);
+			options.splice(3, 6);
 			break;
 			
 			case 3:
@@ -90,13 +90,28 @@ var choice = [];
 			document.getElementById("three").innerHTML = "Very Small";
 			a1 = document.getElementById("three");
 			a1.setAttribute("onclick", "q3(3)");
-			choice.push(3);
-			alert(choice);
+			options.splice(0, 6);
 			break;
-			alert(choice);
 		}
 	}
-	
-	
 
-</script>
+		function q3(answer){
+			switch(answer){
+				case 1:
+				options.splice(1, 3)
+				alert(options);
+				break;
+				
+				case 2:
+				options.splice(0, 1);
+				options.splice(2, 3);
+				alert(options);
+				break;
+				
+				case 3:
+				options.splice(0, 2);
+				alert(options);
+				break;
+			}
+		}	
+	
