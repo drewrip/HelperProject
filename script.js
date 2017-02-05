59d7f99d9929bb2a38729473f06774bd5ddff68d
THE-COB <jmlife6@gmail.com>
2017-02-05 17:07:33 -0500

    Added a restart button

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var options = ["Azio MGK1-K with Blues", "Atreus with Blues", "CM Storm QuickFire TK", "IBM Model M", "Planck with Browns", "IBM Model M SSK", "Corsair STRAFE with Reds", "Planck with Reds", "Corsair Vengeance K65 with Reds", "Das Professional 4 with Blues", "Planck with Blues", "POK3R ANSI US with Blues", "Das Professional 4 with Browns", "Mistel Barocco Ergonomic Split", "POK3R ANSI US with Browns", "Corsair K70", "Planck with Reds", "POK3R ANSI US with Reds", "Matias Tactile Pro with Clicky Switches", "Ergodox Infinity with Greens", "HHKB 2", "Matias Tactile Pro with Clicky Switches", "Ergodox Infinity with zealio swiches", "WhiteFox with Greens", "Cherry MX 6.0 with Reds", "Ergodox Infinity with Reds", "Carpe Keybaords JD45 with Blacks"];
+	var options = ["Azio MGK1-K with Blues", "Atreus with Blues", "CM Storm QuickFire TK", "IBM Model M", "Planck with Browns", "IBM Model M SSK", "Corsair STRAFE with Reds", "Planck with Reds", "Corsair Vengeance K65 with Reds", "Das Professional 4 with Blues", "Planck with Blues", "POK3R ANSI US with Blues", "Das Professional 4 with Browns", "Mistel Barocco Ergonomic Split", "POK3R ANSI US with Browns", "Corsair K70", "Planck with Reds", "POK3R ANSI US with Reds", "Matias Tactile Pro with Clicky Switches", "Ergodox Infinity with Greens", "HHKB 2", "Matias Tactile Pro with Clicky Switches", "Ergodox Infinity with zealio swiches", "WhiteFox with Greens", "Cherry MX 6.0 with Reds", "Ergodox Infinity with Reds", "Carpe Keybaords JD45 with Blacks"];
 	function q1(answer){
 		switch(answer){
 			case 1:
@@ -94,22 +94,39 @@ var options = ["Azio MGK1-K with Blues", "Atreus with Blues", "CM Storm QuickFir
 		}
 	}
 
-		function q3(answer){
-			switch(answer){
-				case 1:
-				options.splice(1, 3)
-				document.getElementById("outputArea").value = options;
-				break;
-				
-				case 2:
-				options.splice(0, 1);
-				options.splice(1, 2);
-				document.getElementById("outputArea").value = options;
-				break;
-				
-				case 3:
-				options.splice(0, 2);
-				document.getElementById("outputArea").value = options;
-				break;
-			}
+	function q3(answer){
+		switch(answer){
+			case 1:
+			options.splice(1, 3)
+			document.getElementById("outputArea").value = options;
+			break;
+			
+			case 2:
+			options.splice(0, 1);
+			options.splice(1, 2);
+			document.getElementById("outputArea").value = options;
+			break;
+			
+			case 3:
+			options.splice(0, 2);
+			document.getElementById("outputArea").value = options;
+			break;
 		}
+	}
+	
+	function restart(){
+		document.getElementById("outputArea").value = "";
+		
+		options = ["Azio MGK1-K with Blues", "Atreus with Blues", "CM Storm QuickFire TK", "IBM Model M", "Planck with Browns", "IBM Model M SSK", "Corsair STRAFE with Reds", "Planck with Reds", "Corsair Vengeance K65 with Reds", "Das Professional 4 with Blues", "Planck with Blues", "POK3R ANSI US with Blues", "Das Professional 4 with Browns", "Mistel Barocco Ergonomic Split", "POK3R ANSI US with Browns", "Corsair K70", "Planck with Reds", "POK3R ANSI US with Reds", "Matias Tactile Pro with Clicky Switches", "Ergodox Infinity with Greens", "HHKB 2", "Matias Tactile Pro with Clicky Switches", "Ergodox Infinity with zealio swiches", "WhiteFox with Greens", "Cherry MX 6.0 with Reds", "Ergodox Infinity with Reds", "Carpe Keybaords JD45 with Blacks"];
+		document.getElementById("question").innerHTML = "What is your price range for a keyboard";
+		
+		a1 = document.getElementById("one");
+		a1.setAttribute("onclick", "q1(1)");
+		document.getElementById("one").innerHTML = "Low";
+		a2 = document.getElementById("two");
+		a2.setAttribute("onclick", "q1(2)");
+		document.getElementById("two").innerHTML = "Medium";
+		a3 = document.getElementById("three");
+		a3.setAttribute("onclick", "q1(3)");
+		document.getElementById("three").innerHTML = "High";
+	}
